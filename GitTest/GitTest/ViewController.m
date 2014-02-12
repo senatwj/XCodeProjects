9f0c5def1a45873cd134ad98c410987cbb50ec40
senatwj <william@Williams-Mac-Pro.local>
2014-02-12 12:23:23 -0500

    xcode git test

--- GitTest/GitTest/ViewController.m
+++ GitTest/GitTest/ViewController.m
@@ -19,7 +19,7 @@
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
     
-    NSLog(@"Application finished loading.");
+    NSLog(@"The application finished loading.");
 }
 
 - (void)didReceiveMemoryWarning
