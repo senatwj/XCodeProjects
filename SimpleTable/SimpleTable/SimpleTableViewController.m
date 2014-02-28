26a5271d95deb6ee8192a2d1462cedcb14b760bc
senatwj <william@Williams-Mac-Pro.local>
2014-02-27 21:12:04 -0500

    fixed checkmark bug

--- SimpleTable/SimpleTable/SimpleTableViewController.m
+++ SimpleTable/SimpleTable/SimpleTableViewController.m
@@ -18,6 +18,7 @@
 NSArray *tableData;
 NSArray *thumbnails;
 NSArray *prepTime;
+NSMutableArray *checkmarked;
 
 - (void)viewDidLoad
 {
@@ -26,6 +27,8 @@ NSArray *prepTime;
     tableData = [NSArray arrayWithObjects:@"Egg Benedict", @"Mushroom Risotto", @"Full Breakfast", @"Hamburger", @"Ham and Egg Sandwich", @"Creme Brelee", @"White Chocolate Donut", @"Starbucks Coffee", @"Vegetable Curry", @"Instant Noodle with Egg", @"Noodle with BBQ Pork", @"Japanese Noodle with Pork", @"Green Tea", @"Thai Shrimp Cake", @"Angry Birds Cake", @"Ham and Cheese Panini", nil];
     thumbnails = [NSArray arrayWithObjects:@"egg_benedict.jpg", @"mushroom_risotto.jpg", @"full_breakfast.jpg", @"hamburger.jpg", @"ham_and_egg_sandwich.jpg", @"creme_brelee.jpg", @"white_chocolate_donut.jpg", @"starbucks_coffee.jpg", @"vegetable_curry.jpg", @"instant_noodle_with_egg.jpg", @"noodle_with_bbq_pork.jpg", @"japanese_noodle_with_pork.jpg", @"green_tea.jpg", @"thai_shrimp_cake.jpg", @"angry_birds_cake.jpg", @"ham_and_cheese_panini.jpg", nil];
     prepTime = [NSArray arrayWithObjects:@"30 min", @"1 hour", @"5 hours", @"20 min", @"10 min", @"30 sec", @"12 days", @"2 hours", @"25 min", @"10 min", @"5 days", @"2 min", @"2 years", @"1 min", @"25 sec", @"45 min", nil];
+    NSNumber *defaultChecked = [NSNumber numberWithInt:0];
+    checkmarked = [NSMutableArray arrayWithObjects:defaultChecked, defaultChecked, defaultChecked, defaultChecked, defaultChecked, defaultChecked, defaultChecked, defaultChecked, defaultChecked, defaultChecked, defaultChecked, defaultChecked, defaultChecked, defaultChecked, defaultChecked, defaultChecked, nil];
 }
 
 - (void)didReceiveMemoryWarning
@@ -50,6 +53,14 @@ NSArray *prepTime;
         cell = [nib objectAtIndex:0];
     }
     
+    NSNumber *isChecked = [checkmarked objectAtIndex:indexPath.row];
+    
+    if ([isChecked intValue] == 0){
+        cell.accessoryType = UITableViewCellAccessoryNone;
+    } else {
+        cell.accessoryType = UITableViewCellAccessoryCheckmark;
+    }
+        
     cell.nameLabel.text = [tableData objectAtIndex:indexPath.row];
     cell.thumbnailImageView.image = [UIImage imageNamed:[thumbnails objectAtIndex:indexPath.row]];
     cell.prepTimeLabel.text = [prepTime objectAtIndex:indexPath.row];
@@ -59,6 +70,31 @@ NSArray *prepTime;
 
 - (CGFloat)tableView:(UITableView *)tableView heightForRowAtIndexPath:(NSIndexPath *)indexPath{
     return 78;
+}
+
+- (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath{
+    
+    NSString *foodName = [tableData objectAtIndex:indexPath.row];
+    
+    UITableViewCell *cell = [tableView cellForRowAtIndexPath:indexPath];
+    
+    NSNumber *isChecked = [checkmarked objectAtIndex:indexPath.row];
+    NSNumber *check;
+    UIAlertView *messageAlert;
+        
+    if ([isChecked intValue] == 0){
+        check = [NSNumber numberWithInt:1];
+        messageAlert = [[UIAlertView alloc] initWithTitle:@"Row Selected" message:foodName delegate:nil cancelButtonTitle:@"Ok" otherButtonTitles:nil];
+        cell.accessoryType = UITableViewCellAccessoryCheckmark;
+    } else {
+        check = [NSNumber numberWithInt:0];
+        messageAlert = [[UIAlertView alloc] initWithTitle:@"Row Deselected" message:foodName delegate:nil cancelButtonTitle:@"Ok" otherButtonTitles:nil];
+        cell.accessoryType = UITableViewCellAccessoryNone;
+    }
+    
+    [checkmarked replaceObjectAtIndex:indexPath.row withObject:check];
+    //[messageAlert show];
+    [tableView deselectRowAtIndexPath:indexPath animated:YES];
 }
 
 @end
